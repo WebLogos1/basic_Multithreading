512057fe845a7c07d84de67686f675bac5d277d1
WebLogos1 <69255518+WebLogos1@users.noreply.github.com>
2021-10-22 08:17:57 -0400

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,49 @@
+// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include<thread>
+
+using namespace std;
+static bool s_Finished = false;
+
+
+void doWork()
+{
+	using namespace literals::chrono_literals;
+
+	while (!s_Finished)
+	{
+
+	cout << "Working....\n";
+	this_thread::sleep_for(.5s);
+	}
+
+
+
+}
+int main()
+{
+	thread worker(doWork);
+	cin.get();
+
+	s_Finished = true;
+
+	worker.join();
+	cout << "Finished!\n";
+	cin.get();
+	
+	
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+//
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+//
